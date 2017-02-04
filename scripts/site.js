fa3133fcfffad1ffd6960bfa355a6cb927b7544f
Joe <joeoh1359@hotmail.com>
2017-02-04 14:32:15 -0500

    first commit

---
+++ scripts/site.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+    setBoxHeight();
+});
+
+$(window).on('resize', function(){
+    setBoxHeight();
+});
+
+function setBoxHeight(){
+    $(".box").each(function(){
+            $(this).height($(this).width());
+        })
+}
+
+function filterBox(filter){
+    console.log(filter);
+    if(filter === undefined){
+        $(".box").each(function(){
+            $(this).css("display","block");
+        })
+    }
+    else{
+        $(".box").each(function(){
+            if($(this).attr("tag").toLowerCase().indexOf(filter)>=0){
+                $(this).css("display", "block");
+            }
+            else{
+                $(this).css("display", "none");
+            }
+        })
+    }
+}
