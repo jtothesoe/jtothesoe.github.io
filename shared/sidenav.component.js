259cf890383f69b4e0a18983fac5d349e59c177d
jtothesoe <jessicahsoe@gmail.com>
2017-02-09 22:12:35 -0500

    Update sidenav.component.js

--- shared/sidenav.component.js
+++ shared/sidenav.component.js
@@ -12,7 +12,6 @@ ko.components.register('sidenav', {
         '<nav class="hidden-sm-down col-md-3 col-xl-2 sidebar row">\
             <div class="col-12 title">\
                 Title\
-              
             </div>\
             <div class="col-12 link-container">\
                 <div data-bind="foreach: navItem"><a data-bind="attr:{href: url}, text: text"></a></div>\
