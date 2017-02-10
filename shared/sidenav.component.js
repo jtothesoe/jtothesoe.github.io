b79d4c24d1a13dbf09c764dd7ac9215606ae6caa
jtothesoe <jessicahsoe@gmail.com>
2017-02-10 05:52:07 -0500

    Update sidenav.component.js

--- shared/sidenav.component.js
+++ shared/sidenav.component.js
@@ -13,7 +13,7 @@ ko.components.register('sidenav', {
             <div class="col-12 title">\
                 Title\
             </div>\
-<img src="http://imgur.com/P5PGQOj" alt="logo">\
+<img src="http://i.imgur.com/P5PGQOj.png" alt="logo">\
             <div class="col-12 link-container">\
                 <div data-bind="foreach: navItem"><a data-bind="attr:{href: url}, text: text"></a></div>\
                 <a>Say Hello</a>\
