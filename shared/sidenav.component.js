53c680dad3690476bb67c1a167b2327f32705dcb
jtothesoe <jessicahsoe@gmail.com>
2017-02-09 22:08:30 -0500

    Update sidenav.component.js

--- shared/sidenav.component.js
+++ shared/sidenav.component.js
@@ -12,6 +12,7 @@ ko.components.register('sidenav', {
         '<nav class="hidden-sm-down col-md-3 col-xl-2 sidebar row">\
             <div class="col-12 title">\
                 Title\
+                <img src="http://i.imgur.com/NNL6ahm.png" alt="Mountain View" style="width:304px;height:228px;">
             </div>\
             <div class="col-12 link-container">\
                 <div data-bind="foreach: navItem"><a data-bind="attr:{href: url}, text: text"></a></div>\
