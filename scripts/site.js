4fcd062afff8a89d57bca1c1c28ebde95c5457d3
joeoh1359 <joseph.sh.oh+github@gmail.com>
2017-02-12 16:00:37 -0500

    Update site.js

--- scripts/site.js
+++ scripts/site.js
@@ -1,16 +1,22 @@
 $(document).ready(function() {
     setBoxHeight();
+    setBackgroundTextHeight();
     w3IncludeHTML();
 });
 
 $(window).on('resize', function(){
     setBoxHeight();
+    setBackgroundTextHeight();
 });
 
 function setBoxHeight(){
     $(".box").each(function(){
             $(this).height($(this).width());
         })
+}
+
+function setBackgroundTextHeight(){
+    $("#background-text-spacer").height($("#background-text-area").height());
 }
 
 function filterBox(filter){
