de3a2f4b8c3d60eaed24c6714e9562692a12631f
joeoh1359 <joseph.sh.oh+github@gmail.com>
2017-02-12 16:29:26 -0500

    Update sidenav.component.js

--- shared/sidenav.component.js
+++ shared/sidenav.component.js
@@ -14,8 +14,10 @@ ko.components.register('sidenav', {
                 <img src="http://i.imgur.com/P5PGQOj.png?4" alt="logo" style="width:50px;height:102px;" align="middle" hspace"5">\
             </div>\
             <div class="col-12 link-container">\
-                <div class="flex-container" style="flex-direction:row" data-bind="foreach: navItem"><div><a data-bind="attr:{href: url}, text: text"></a></div></div>\
-                <div class="link-container">\  <a>2017 ©  Jessica Soe </a> </div>\
+                <div class="flex-container" style="width:100%; flex-direction:row" data-bind="foreach: navItem"><div><a data-bind="attr:{href: url}, text: text"></a></div></div>\
+            </div>\
+            <div class="col-12 link-container">\
+                <div style="width:100%">\  <a>2017 ©  Jessica Soe </a> </div>\
                 <div class="social-profile">\
                     <ul>\
                         <li><a href="#"><i class="fa fa-facebook"></i></a></li>\
