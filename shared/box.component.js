588e1acab83d853859a7bafa1942a1ebdf6b9f48
Joe <joeoh1359@hotmail.com>
2017-02-15 21:45:13 -0500

    test to get box working

--- shared/box.component.js
+++ shared/box.component.js
@@ -1,20 +1,21 @@
+
 ko.components.register('box', {
-    viewModel: function (params) {
-        this.id = params.id;
-        this.tags = params.tags;
-        this.title = params.title;
-        this.description = params.description;
-        this.imgUrl = params.imgUrl;
-        this.projectUrl = params.projectUrl;
+    viewModel: function (id, tags, title, description, imgUrl, projectUrl) {
+        this.id = id;
+        this.tags = tags;
+        this.title = title;
+        this.description = description;
+        this.imgUrl = imgUrl;
+        this.projectUrl = projectUrl;
     },
     template:
-    '<a class="col-sm-12 col-md-6 col-lg-4 col-xl-3 box" data-bind="id:id, tag:tags, href:projectUrl">\
+    '<a class="col-sm-12 col-md-6 col-lg-4 col-xl-3 box" data-bind="attr:{id:id, tag:tags, href:projectUrl}">\
         <div class="content">\
             <div class="content-hover">\
-                <h4 class="font2" data-bind="value:title"></h4>\
+                <h4 class="font2" data-bind="text:title"></h4>\
                 <div class="box-line" style="padding-bottom:25px"></div>\
                 <h5>\
-                    <div class="padding-top:25px" data-bind="value:description">\
+                    <div class="padding-top:25px" data-bind="text:description">\
                     </div>\
                 </h5>\
             </div>\
--- shared/sidenav.component.js
+++ shared/sidenav.component.js
@@ -30,29 +30,4 @@ ko.components.register('sidenav', {
         </nav>'
 });
 
-ko.components.register('box', {
-    viewModel: function (id, tags, title, description, imgUrl, projectUrl) {
-        this.id = id;
-        this.tags = tags;
-        this.title = title;
-        this.description = description;
-        this.imgUrl = imgUrl;
-        this.projectUrl = projectUrl;
-    },
-    template:
-    '<a class="col-sm-12 col-md-6 col-lg-4 col-xl-3 box" data-bind="attr:{id:id, tag:tags, href:projectUrl}">\
-        <div class="content">\
-            <div class="content-hover">\
-                <h4 class="font2" data-bind="text:title"></h4>\
-                <div class="box-line" style="padding-bottom:25px"></div>\
-                <h5>\
-                    <div class="padding-top:25px" data-bind="text:description">\
-                    </div>\
-                </h5>\
-            </div>\
-            <img data-bind="src: imgUrl" />\
-        </div>\
-    </a>'
-});
-
 ko.applyBindings();
