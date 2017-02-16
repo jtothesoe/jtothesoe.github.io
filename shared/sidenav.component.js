0ce45bded63daeede69d5ff4cdaf26642829832a
Joe <joeoh1359@hotmail.com>
2017-02-15 21:15:41 -0500

    possible temporary workaround to ko binding multiple components

--- shared/sidenav.component.js
+++ shared/sidenav.component.js
@@ -30,4 +30,29 @@ ko.components.register('sidenav', {
         </nav>'
 });
 
+ko.components.register('box', {
+    viewModel: function (params) {
+        this.id = params.id;
+        this.tags = params.tags;
+        this.title = params.title;
+        this.description = params.description;
+        this.imgUrl = params.imgUrl;
+        this.projectUrl = params.projectUrl;
+    },
+    template:
+    '<a class="col-sm-12 col-md-6 col-lg-4 col-xl-3 box" data-bind="id:id, tag:tags, href:projectUrl">\
+        <div class="content">\
+            <div class="content-hover">\
+                <h4 class="font2" data-bind="value:title"></h4>\
+                <div class="box-line" style="padding-bottom:25px"></div>\
+                <h5>\
+                    <div class="padding-top:25px" data-bind="value:description">\
+                    </div>\
+                </h5>\
+            </div>\
+            <img data-bind="src: imgUrl" />\
+        </div>\
+    </a>'
+});
+
 ko.applyBindings();
