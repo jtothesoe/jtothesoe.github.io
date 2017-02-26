e62660c3b4c2053ae2646e7b0498c9929bb4f638
jtothesoe <jessicahsoe@gmail.com>
2017-02-25 22:18:37 -0500

    Update sidenav.component.js

--- shared/sidenav.component.js
+++ shared/sidenav.component.js
@@ -16,8 +16,8 @@ ko.components.register('sidenav', {
             <div class="col-12 link-container">\
                 <div class="flex-container" style="width:100%; flex-direction:row" data-bind="foreach: navItem"><div><a style="color:#3498db" data-bind="attr:{href: url}, text: text"></a></div></div>\
             </div>\
-            <div class="col-12 link-container">\
-                <div class="social-profile" style="flex-direction:column-reverse !important">\
+            <div class="col-12 link-container" style="flex-direction:column-reverse !important">\
+                <div class="social-profile">\
                     <ul>\
                         <li><a href="https://www.linkedin.com/in/jessicasoe/"><i class="fa fa-linkedin"></i></a></li>\
                         <li><a href="#"><i class="fa fa-dribbble"></i></a></li>\
